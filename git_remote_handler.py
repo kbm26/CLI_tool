a3e139fecb7f72afc63eaddd16bce02fdbfa2b51
kymoodl022 <kymoodl022@student.wethinkcode.co.za>
2023-10-11 14:42:54 +0200

    git methods created

--- git_remote_handler.py
+++ git_remote_handler.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from github import Github
 from github import Auth
 import subprocess
+from InquirerPy import inquirer
 
 
 def credentials_validator() -> bool:
@@ -17,11 +18,11 @@ def credentials_validator() -> bool:
     """
     try:
         directory = os.path.dirname(".secrets/creds.json")
-        with open(directory,"r+") as f:
+        with open(f"{directory}/creds.json","r+") as f:
             user:dict = json.load(f)
         user.get("creds")
         return True
-    except e :
+    except Exception :
         return False
     
 def create_credentials() -> None:
@@ -29,32 +30,120 @@ def create_credentials() -> None:
     Asks user for GitHub Access Token
     then creates a folder and file for the user's token
     """
+    subprocess.run(["mkdir","./.secrets"])
+    write_token()
+        
+def write_token() -> None:
+    """
+    write the token to the creds json
+    """
     token = input("Enter Your GitHub Access Token: ")
-    # subprocess.run(["mkdir",".secrets"])
+    subprocess.run(["mkdir",".secrets"])
     creds = {
         "token": token,
     }
     
     with open(".secrets/creds.json", "w") as outfile:
         json.dump(creds, outfile)
-        
-    def show_all_repos() -> None:
-    # auth = Auth.Login("")
-
-    # g = Github(auth=auth)
-
-    # g.get_user().login
-
-    # # Github Enterprise with custom hostname
-
-    # for repo in g.get_user().get_repos():
-    #     print(repo.name)
-    #     # to see all the available attributes and methods
-    #     print(dir(repo))
-
-    # g.close()
-        pass
-
     
+def show_all_repos(git:Github) -> None:
+    """Displays all repos under a user's name from github
+
+    Args:
+        git (Github): Access object to execute methods
+    """
+    for repo in git.get_user().get_repos():
+        print(repo.name)
+        
+    git_close(git)
+        
+def git_login() -> Github:
+    """Logins to user's github account
+
+    Returns:
+        Github: Access object to execute methods
+    """
+    directory = os.path.dirname(".secrets/creds.json")
+    with open(f"{directory}/creds.json","r+") as f:
+        user:dict = json.load(f)
+    creds = user.get("token")
+    auth = Auth.Token(creds)
+    git = Github(auth=auth)
+    git.get_user().login
+    
+    return git
+    
+def git_close(git:Github) :
+    """Closes the github access object for security reasons
+
+    Args:
+        git (Github): Access object to execute methods
+    """
+    git.close()
+    
+    
+def change_issue_label(git:Github, label:str) -> None:
+    """Changes the label of a single commit from a repo
+
+    Args:
+        git (Github): Access object to execute methods
+        label (str): The new name of the label for the issue
+    """
+    # Will change
+    for repo in git.get_user().get_repos():
+        print(repo.name)
+        for issue in repo.get_issues():
+            print(issue.set_labels("completed"))
+            
+    git_close(git)
+    
+    
+def create_repository(git:Github, repo_name:str) -> None:
+    """Creates a repository under the logged in user
+
+    Args:
+        git (Github): Access object to execute methods
+        repo_name (str): The name of the repo that the user wants to make
+    """
+    user = git.get_user()
+    user.create_repo(repo_name)
+    
+    
+def delete_repository(git:Github, repo_name:str) -> None:
+    """Deletes a repository under the logged in user
+
+    Args:
+        git (Github): Access object to execute methods
+        repo_name (str): The name of the repo that the user wants to delete
+    """
+    user = git.get_user()
+    repo = user.get_repo(repo_name)
+    repo.delete()
+    
+    
+def create_issues(git:Github, repo_name:str , title:str , body:str="" ,label:str="", assignee:str="" ):
+    user = git.get_user()
+    repo = user.get_repo(repo_name)
+    repo.create_issue(title=title,body=body,assignee=assignee)
+    repo.get_issue(repo.get_issues().totalCount).set_labels(label)
+    git_close(git)
+    
+def close_issues(git:Github, repo_name:str ,issue_number:int ):
+    user = git.get_user()
+    repo = user.get_repo(repo_name)
+    repo.get_issue(issue_number)
+
+    git_close(git)
+
+
+def edit_issues(git:Github, repo_name:str ,issue_number:int, title:str , body:str="" ,label:str="", assignee:str=""):
+    user = git.get_user()
+    repo = user.get_repo(repo_name)
+    repo.create_issue(title=title,body=body,assignee=assignee)
+    repo.get_issue(issue_number).set_labels(label)
+    
+    git_close(git)
+
+
 if __name__ == '__main__':
-    create_credentials()
+    create_issues(git_login(),"CLI_tool","test","here is a test","backlog")
